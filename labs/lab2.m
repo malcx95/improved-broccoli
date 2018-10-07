389acddfcff3948a6d5184ca86f4adc321370aec
TheZoq2 <frans.skarman@gmail.com>
2018-10-07 15:01:17 +0200

    Matlab should be illegal

--- labs/lab2.m
+++ labs/lab2.m
@@ -1,45 +1,46 @@
-A = [
-0.8 6.6 10.2;
-2 	4 	8;
--1.2 -1.9 -3.1;
-];
-
-[L, U, P] = lu(A);
-
-LU = L*U;
-
-%% Uppgift 2.1
-
-b = [5 0 0.5]';
-y = fram(L, P*b);
-x = bak(U, y);
-
-%% Uppgift 3.1
-
-last_inv = 1;
-last_back = 1;
-for i=[1, 2, 4, 8]
-    n = 1000*i
-    A = rand(n);
-    b = rand(n, 1);
-
-    tic;
-    x = inv(A)*b;
-    time = toc;
-    yolo = time / last_inv;
-    last_inv = time;
-    fprintf('n = %d, last_inv = %d time = %d yolo = %d\n', n, last_inv, time, yolo);
-    tic;
-    x = A\b;
-    time = toc;
-    yolo = time / last_back;
-    last_back = time;
-    fprintf('n = %d, last_back = %d time = %d yolo = %d\n', n, last_back, time, yolo);
-end
+% A = [
+% 0.8 6.6 10.2;
+% 2 	4 	8;
+% -1.2 -1.9 -3.1;
+% ];
+% 
+% [L, U, P] = lu(A);
+% 
+% LU = L*U;
+% 
+% %% Uppgift 2.1
+% 
+% b = [5 0 0.5]';
+% y = fram(L, P*b);
+% x = bak(U, y);
+% 
+% %% Uppgift 3.1
+% 
+% last_inv = 1;
+% last_back = 1;
+% for i=[1, 2, 4, 8]
+%     n = 1000*i
+%     A = rand(n);
+%     b = rand(n, 1);
+% 
+%     tic;
+%     x = inv(A)*b;
+%     time = toc;
+%     yolo = time / last_inv;
+%     last_inv = time;
+%     fprintf('n = %d, last_inv = %d time = %d yolo = %d\n', n, last_inv, time, yolo);
+%     tic;
+%     x = A\b;
+%     time = toc;
+%     yolo = time / last_back;
+%     last_back = time;
+%     fprintf('n = %d, last_back = %d time = %d yolo = %d\n', n, last_back, time, yolo);
+% end
 
 
 
 %%
+
 
 A = [10^-13 1 0 0;
     1 2 1 0;
@@ -62,5 +63,9 @@ b = x.^2;
 c = A\b;
 
 figure
-ezplot('-1.2*y^2 + 0.45*2*x*y + 5.7*x - 0.88*y - x^2')
+hold on
+ezplot('-1.2*y^ 2 + 0.45*x*y + 5.7*x - 0.88*y - x^2')
+
+ceres
+plot(x, y, 'r+')
 
---
+++ labs/lab4.m
@@ -0,0 +1,72 @@
+% 2.2
+
+% f = @(x, y) -5 * y + 2.5;
+% 
+% solution = @(x) (exp(-5 * x) + 1) / 2;
+% 
+% for h = [0.1, 0.45, 0.6]
+%     x0 = 0;
+%     y0 = 1;
+%     xn = 5;
+%     figure(round(h * 200));
+%     hold on;
+%     subplot(2,1,1);
+%     plot([x0:0.01:xn], solution([x0:0.01:xn]));
+%     myeuler(f, x0, xn, y0, h);
+%     title(sprintf('eulers h = %f', h));
+% 
+%     subplot(2,1,2);
+%     plot([x0:0.01:xn], solution([x0:0.01:xn]));
+%     myrungekutta(f, x0, xn, y0, h);
+%     title(sprintf('runges h = %f', h));
+% end
+
+
+
+
+% 3.2
+
+ t0 = 0;
+ tn = 500;
+ v0 = [-1, 0.001, 0]';
+
+% for h = [1.5, 2.1, 2.24, 2.26]
+%     x0 = 0;
+%     y0 = 1;
+%     xn = 5;
+%     figure(round(h * 200));
+%     myeuler(@odefun, t0, tn, v0, h);
+%     title(sprintf('runges h = %f', h));
+% end
+
+
+tn = 100;
+h = 0.2;
+full = myeuler(@odefun, t0, tn, v0, h);
+while 1
+     h = h/2;
+     y0 = 1;
+
+     half = myeuler(@odefun, t0, tn, v0, h);
+
+     if(abs(full - half) < 10^-5) 
+         disp(h);
+         disp(half);
+         disp(full - half);
+         break
+     end
+
+     full = half;
+     disp("din mamma")
+end
+
+
+
+for Ki = [0.01, 0.05, 0.125]
+    figure(Ki * 10000)
+    [t,v] = ode23(@(x,y) odefun_ki(Ki, x, y), [t0, tn], v0);
+    disp(t(end))
+
+    disp(v(end))
+end
+
