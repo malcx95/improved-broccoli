d45660ce6b5ac47840965789d9f49ca5eb9c5800
Malcolm Wigren <trekommafem2@gmail.com>
2018-10-16 16:24:49 +0200

    matlab

---
+++ project2/main.m
@@ -0,0 +1,40 @@
+second_deg = @(x) x^2 - 2;
+second_deg_der = @(x) 2*x;
+
+tan_f = @(x) tan(x);
+tan_f_der = @(x) 1/(1 + x^2);
+
+sin_f = @(x) sin(x);
+sin_f_der = @(x) cos(x);
+
+funcs = {{second_deg, second_deg_der, 1, 2, 'x^2 - 2', sqrt(2)},...
+        {tan_f, tan_f_der, -0.3, 0.5, 'tan(x)', 0},...
+        {sin_f, sin_f_der, -0.4, 0.5, 'sin(x)', 0}};
+
+for cell = funcs
+    c = cell{1};
+    f = c{1};
+    fp = c{2};
+    x0 = c{3};
+    x1 = c{4};
+    text = c{5};
+    correct = c{6};
+    
+    [root, sols] = sekant(f, fp, x0, x1, 1e-10);
+
+    errs = abs(sols - correct);
+    range = 1:(length(errs)-1);
+
+    ps = [];
+    for i = range
+        p = (log(errs(i))/log(errs(i + 1)));
+        ps = [ps p];
+    end
+
+    fprintf(text);
+    fprintf(': Root: %d. p = ', root);
+    fprintf('%d, ', ps);
+    fprintf('\n\n\n\n');
+end
+
+
--- project2/sekant.m
+++ project2/sekant.m
@@ -1,16 +1,20 @@
-function root = sekant(f, x0, precision)
+function [root, sols] = sekant(f, fp, x0, x1, precision)
 
 err = Inf;
-x_prev = x0 - precision;
-x = x0;
+x_prev = x0;
+x = x1;
+
+sols = [];
 
 while err > precision
 
     derivative = (f(x) - f(x_prev))/(x - x_prev);
     next = x - f(x)/derivative;
     x_prev = x;
-    err = abs(x - next);
     x = next;
+    err = abs(f(x)/fp(x));
+
+    sols = [sols x];
 
 end
 root = x;
